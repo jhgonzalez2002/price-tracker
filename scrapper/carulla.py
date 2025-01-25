2160b1a11db8b9af51cd7e2341c146540d7b01ac
jhgonzalez2002 <164440357+jhgonzalez2002@users.noreply.github.com>
2025-01-25 13:05:44 -0500

    Update carulla.py

--- scrapper/carulla.py
+++ scrapper/carulla.py
@@ -1,10 +1,11 @@
 """Definition for CarullaScrapper class."""
-
+# Import the SharedScrapper class from the shared_scrapper module within the scrapper package.
 from scrapper.shared_scrapper import SharedScrapper
 
-
+#Define the CarullaScrapper class that inherits from the SharedScrapper class.
 class CarullaScrapper(SharedScrapper):
     """Class to fetch prices from www.carulla.com"""
-
+    #Constructor method for the CarullaScrapper class.
     def __init__(self):
+        #Call the constructor of the base class and pass the URL of the website to be scrapped.
         super().__init__("www.carulla.com")
